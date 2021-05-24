287b5b1b8001cc4df7aab40c6ac1a3f714036406
markeezy <markkhoo95@gmail.com>
2021-05-23 23:45:02 -0700

    waves to footer

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route} from 'react-router-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Header from './components/header/header';
 import Footer from './components/footer/footer';
 import Contact from './pages/contact/contact';
@@ -12,9 +12,11 @@ function App() {
     <Router>
       <Header />
       <div className="main">
-        <Route exact path='/' component={Home}/>
-        <Route exact path='/contact' component={Contact}/>
-        <Route exact path='/projects' component={Projects}/>
+        <Route exact path='/' component={Home} />
+        <Route exact path='/contact' component={Contact} />
+        <Route exact path='/projects' component={Projects} />
+
+
       </div>
       <Footer />
     </Router>
--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -1,11 +1,28 @@
 import React from 'react';
 import './footer.css';
 
-function Footer(){
+function Footer() {
     return (
         <div className="footer">
-            <p></p>
-            <p>2021 /// Powered with <a href="https://reactjs.org/">React</a></p>
+            <p>Waves by <a href="https://codepen.io/goodkatz/pen/LYPGxQz?editors=1100/">Daniel Österman</a></p>
+            <p>2021</p>
+            <p>Powered with <a href="https://reactjs.org/">React</a></p>
+
+            {/* React needs svg Namespace tag conversions: (shape-rendering TO shapeRendering) & ( xmlns:xlink TO  xmlnsXlink ) & ( xlink:href TO xlinkHref ) https://stackoverflow.com/questions/59820954/syntaxerror-unknown-namespace-tags-are-not-supported-by-default */}
+            <div id="background-container">
+                <svg className="waves" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink"
+                    viewBox="0 24 150 28" preserveAspectRatio="none" shapeRendering="auto">
+                    <defs>
+                        <path id="gentle-wave" d="M-160 44c30 0 58-18 88-18s 58 18 88 18 58-18 88-18 58 18 88 18 v44h-352z" />
+                    </defs>
+                    <g className="parallax">
+                        <use xlinkHref="#gentle-wave" x="48" y="0" fill="rgba(53, 74, 95, 0.7)" />
+                        <use xlinkHref="#gentle-wave" x="48" y="3" fill="rgba(53, 74, 95, 0.5)" />
+                        <use xlinkHref="#gentle-wave" x="48" y="5" fill="rgba(53, 74, 95, 0.3)" />
+                        <use xlinkHref="#gentle-wave" x="48" y="7" fill="rgba(53, 74, 95, 1)" />
+                    </g>
+                </svg>
+            </div>
         </div>
     )
 };
