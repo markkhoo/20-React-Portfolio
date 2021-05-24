c8fee1a771884f345576e0c9b134cf6f29dc5967
markeezy <markkhoo95@gmail.com>
2021-05-24 00:01:53 -0700

    home styling

--- src/App.js
+++ src/App.js
@@ -15,8 +15,6 @@ function App() {
         <Route exact path='/' component={Home} />
         <Route exact path='/contact' component={Contact} />
         <Route exact path='/projects' component={Projects} />
-
-
       </div>
       <Footer />
     </Router>
