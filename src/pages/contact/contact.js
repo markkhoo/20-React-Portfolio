cec35d62123b96ea2fb9fdc4ac9dfb968659cf09
markeezy <markkhoo95@gmail.com>
2021-05-24 15:17:13 -0700

    compress imgs and fic accessability in contact

--- src/pages/contact/contact.js
+++ src/pages/contact/contact.js
@@ -4,12 +4,14 @@ import './contact.css';
 function Contact(){
     return (
         <div className="contact">
-            <a id="email" href="mailto:markkhoo95@gmail.com">{String.fromCharCode(32)}</a>
-            <a id="linkd" href="https://www.linkedin.com/in/markdkhoo/" target="_blank" rel="noreferrer noopener">{String.fromCharCode(32)}</a>
-            <a id="githb" href="https://github.com/markkhoo" target="_blank" rel="noreferrer noopener">{String.fromCharCode(32)}</a>
+            <a id="email" href="mailto:markkhoo95@gmail.com">Email</a>
+            <a id="linkd" href="https://www.linkedin.com/in/markdkhoo/" target="_blank" rel="noreferrer noopener">LinkedIn</a>
+            <a id="githb" href="https://github.com/markkhoo" target="_blank" rel="noreferrer noopener">GitHub</a>
             <a id="resum" href="https://drive.google.com/file/d/1GAoY4YpH1W7qby-to_XkX3vl1zGOhbp5/view?usp=sharing" target="_blank" rel="noreferrer noopener">RESUME</a>
         </div>
     )
 };
 
 export default Contact;
+
+// Empty Character: {String.fromCharCode(32)}
