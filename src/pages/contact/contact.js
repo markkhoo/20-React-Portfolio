4e0d302e6e2a96a6d3d26a7dbcf9d686d5d9a33a
markeezy <markkhoo95@gmail.com>
2021-05-24 00:37:32 -0700

    correct display of days since begin

--- src/pages/contact/contact.js
+++ src/pages/contact/contact.js
@@ -4,9 +4,9 @@ import './contact.css';
 function Contact(){
     return (
         <div className="contact">
-            <a id="email" href="mailto:markkhoo95@gmail.com"></a>
-            <a id="linkd" href="https://www.linkedin.com/in/markdkhoo/" target="_blank" rel="noreferrer noopener"></a>
-            <a id="githb" href="https://github.com/markkhoo" target="_blank" rel="noreferrer noopener"></a>
+            <a id="email" href="mailto:markkhoo95@gmail.com">{String.fromCharCode(32)}</a>
+            <a id="linkd" href="https://www.linkedin.com/in/markdkhoo/" target="_blank" rel="noreferrer noopener">{String.fromCharCode(32)}</a>
+            <a id="githb" href="https://github.com/markkhoo" target="_blank" rel="noreferrer noopener">{String.fromCharCode(32)}</a>
             <a id="resum" href="https://drive.google.com/file/d/1GAoY4YpH1W7qby-to_XkX3vl1zGOhbp5/view?usp=sharing" target="_blank" rel="noreferrer noopener">RESUME</a>
         </div>
     )
--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -1,22 +1,23 @@
 import React, { useState, useEffect } from 'react';
-// import DayJS from 'react-dayjs';
 import './home.css';
 import ProfilePic from './MarkKhoo.png';
 
 function Home(){
-    const [getDays, setDays] = useState(0);
+    const [getDays, setDays] = useState("???");
 
-    // useEffect(() => {
-
-    // }, [getDays]);
-
-    // const interval = setInterval(() => {
-    //     setDays(dayjs().diff(dayjs(1615262400000),'day',true));
-    // },100);
+    useEffect(() => {
+        const d = new Date().getTime();
+        setDays(Math.floor((d - 1615262400000) / (24*60*60*1000)));
+        const interval = setInterval(() => {
+            setDays(Math.floor((d - 1615262400000) / (24*60*60*1000)));
+        },1000);
+        return () => clearInterval(interval)
+        // https://upmostly.com/tutorials/setinterval-in-react-components-using-hooks
+    }, [])
 
     return (
         <div className="home">
-            <img src={ProfilePic} alt="Profile Photo"/>
+            <img src={ProfilePic} alt="My Headshot"/>
             <p>
                 I am a former Construction Engineer EIT based out in Emeryville, California. I was last seen designing Falsework for the Los Angeles County Museum of Art that spans over Wilshire Boulevard. Now, I am an up and coming full stack developer. It has been <span>{getDays}</span> days since I began my journey coding. Coding really scratches that creative and problem-solving itch. I strive to be, at the very least, a full-stack dilettante and insatialble learner. Where will I be next?
             </p>
