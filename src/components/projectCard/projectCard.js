b09c9efb83d1b82ce2e86fbba8b1ad7d9439d649
markeezy <markkhoo95@gmail.com>
2021-05-24 21:31:39 -0700

    projects page WIP styling

--- src/components/projectCard/projectCard.js
+++ src/components/projectCard/projectCard.js
@@ -1,1 +1,14 @@
-//
+import React from 'react';
+import './projectCard.css';
+
+function Card(props) {
+    return(
+        <div className="card" id={props.id}>
+            <h2>{props.title}</h2>
+            <a href={props.deploy_link} target="_blank" rel="noreferrer noopener">Deployed</a>
+            <a href={props.github_link} target="_blank" rel="noreferrer noopener">GitHub</a>
+        </div>
+    )
+};
+
+export default Card;
--- src/pages/projects/projects.js
+++ src/pages/projects/projects.js
@@ -1,10 +1,60 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Card from '../../components/projectCard/projectCard';
 import './projects.css';
 
 function Projects(){
+    const [getProj, setProj] = useState([
+        {
+            id: 1,
+            title: "Sporcle-But-Better",
+            deploy_link: "https://github.com/markkhoo/Sporcle-But-Better",
+            github_link: "https://radiant-chamber-32939.herokuapp.com/",
+        },
+        {
+            id: 2,
+            title: "Trail-Scout",
+            deploy_link: "https://github.com/markkhoo/Trail-Scout",
+            github_link: "https://markkhoo.github.io/Trail-Scout/",
+        },
+        {
+            id: 3,
+            title: "Employee-Director",
+            deploy_link: "https://github.com/markkhoo/19-employee-directory",
+            github_link: "https://markkhoo.github.io/19-employee-directory/",
+        },
+        {
+            id: 4,
+            title: "Clicky-Game",
+            deploy_link: "https://github.com/markkhoo/18-Clicky-Game",
+            github_link: "https://markkhoo.github.io/18-Clicky-Game/",
+        },
+        {
+            id: 5,
+            title: "Note-Taker",
+            deploy_link: "https://github.com/markkhoo/11-Note-Taker-Express.js",
+            github_link: "https://mighty-dusk-98751.herokuapp.com/",
+        },
+        {
+            id: 6,
+            title: "Quiz-Game",
+            deploy_link: "https://github.com/markkhoo/04-Code-Quiz-Assignment",
+            github_link: "https://markkhoo.github.io/04-Code-Quiz-Assignment/",
+        }
+    ]);
+
     return (
         <div className="projects">
-            <p>projects</p>
+            {getProj.map(item => {
+                return (
+                    <Card 
+                        key={item.id}
+                        id={item.id}
+                        title={item.title}
+                        deploy_link={item.deploy_link}
+                        github={item.github_link}
+                    />
+                )
+            })}
         </div>
     )
 };
