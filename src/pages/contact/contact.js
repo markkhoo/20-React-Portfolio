09d7d38afe51c6544be4a66a361c6feed95ba978
markeezy <markkhoo95@gmail.com>
2021-08-02 14:48:56 -0700

    update resume link

--- src/pages/contact/contact.js
+++ src/pages/contact/contact.js
@@ -7,7 +7,7 @@ function Contact(){
             <a id="email" href="mailto:markkhoo95@gmail.com">Email</a>
             <a id="linkd" href="https://www.linkedin.com/in/markdkhoo/" target="_blank" rel="noreferrer noopener">LinkedIn</a>
             <a id="githb" href="https://github.com/markkhoo" target="_blank" rel="noreferrer noopener">GitHub</a>
-            <a id="resum" href="https://drive.google.com/file/d/1JwhgLJOY85eWk2_apCtkArAgrdMCCsm0/view?usp=sharing" target="_blank" rel="noreferrer noopener">RESUME</a>
+            <a id="resum" href="https://drive.google.com/file/d/141ssyk9tqywB5Agyf_wq18wM_newu06J/view?usp=sharing" target="_blank" rel="noreferrer noopener">RESUME</a>
         </div>
     )
 };
